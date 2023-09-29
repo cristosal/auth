f713d854592b954dc15481f1da76940795088267
cristobalsalazar <chris.salza@gmail.com>
2023-09-28 20:54:13 -0700

    update limiter

--- rate_limiter.go
+++ rate_limiter.go
@@ -9,35 +9,56 @@ import (
 	"github.com/go-redis/redis/v7"
 )
 
-// MaxAttemptsError should be returned by Limit implementations when the maximum number of attempts have been hit
+// MaxAttemptsError is returned by Limit implementations when the maximum number of attempts have been hit
 type MaxAttemptsError struct {
-	TTL      time.Duration
-	Attempts int
+	TTL      time.Duration // time until window becomes available
+	Attempts int           // attempt limit that was reached
 }
 
+// As satisfies the As interface for use with errors.As
+func (e *MaxAttemptsError) As(v interface{}) bool {
+	if err, ok := v.(*MaxAttemptsError); ok {
+		err.Attempts = e.Attempts
+		err.TTL = e.TTL
+		return true
+	}
+
+	return false
+}
+
+// Error implementation
 func (err MaxAttemptsError) Error() string {
 	return fmt.Sprintf("max attempts: ttl %s", err.TTL)
 }
 
 // Limiter defines the interface for rate limiting
 type Limiter interface {
-	Limit(key string, maxAttempts int, expiration time.Duration) error
+	// Limit increases an internal counter everytime it is called.
+	// When the counter reaches the value specified by max within a given time window, MaxAttemptsError is returned.
+	// The error is returned until the time duration specifed by window has elapsed.
+	Limit(key string, max int, window time.Duration) error
+
+	// Reset resets the internal counter to 0
 	Reset(key string) error
 }
 
-// RedisLimiter is the implementation for Limiter using redis as cache
-type RedisLimiter struct {
-	Client *redis.Client
+// redisLimiter is the implementation for Limiter using redis as cache
+type redisLimiter struct{ Client *redis.Client }
+
+func NewRedisRateLimiter(cl *redis.Client) Limiter {
+	return &redisLimiter{cl}
 }
 
 // Limit is the implementation of Limiter interface
-func (l RedisLimiter) Limit(key string, maxAttempts int, expiration time.Duration) error {
+func (l redisLimiter) Limit(key string, max int, window time.Duration) error {
 	hits, err := l.get(key)
 	if err != nil {
 		return err
 	}
 
-	if hits >= maxAttempts {
+	// see if we have hit the limit
+	if hits >= max {
+		// read ttl to send back to client
 		ttl, err := l.ttl(key)
 		if err != nil {
 			return err
@@ -46,31 +67,26 @@ func (l RedisLimiter) Limit(key string, maxAttempts int, expiration time.Duratio
 		return MaxAttemptsError{ttl, hits}
 	}
 
-	hits, err = l.hit(key)
-
+	// we hit the cache
+	_, err = l.hit(key)
 	if err != nil {
 		return err
 	}
 
-	if hits >= maxAttempts {
-		err = l.expire(key, expiration)
-
-		if err != nil {
-			return err
-		}
-
-		return MaxAttemptsError{expiration, hits}
+	// and update the expiry window
+	if err := l.expire(key, window); err != nil {
+		return err
 	}
 
 	return nil
 }
 
-func (l RedisLimiter) Reset(key string) error {
+func (l redisLimiter) Reset(key string) error {
 	cmd := l.Client.Del(key)
 	return cmd.Err()
 }
 
-func (l RedisLimiter) get(key string) (hits int, err error) {
+func (l redisLimiter) get(key string) (hits int, err error) {
 	cmd := l.Client.Get(key)
 	err = cmd.Err()
 
@@ -85,18 +101,18 @@ func (l RedisLimiter) get(key string) (hits int, err error) {
 	return strconv.Atoi(cmd.Val())
 }
 
-func (l RedisLimiter) hit(key string) (hits int, err error) {
+func (l redisLimiter) hit(key string) (hits int, err error) {
 	cmd := l.Client.Incr(key)
 	err = cmd.Err()
 	hits = int(cmd.Val())
 	return
 }
 
-func (l RedisLimiter) expire(key string, duration time.Duration) error {
+func (l redisLimiter) expire(key string, duration time.Duration) error {
 	cmd := l.Client.Expire(key, duration)
 	return cmd.Err()
 }
 
-func (l RedisLimiter) ttl(key string) (time.Duration, error) {
+func (l redisLimiter) ttl(key string) (time.Duration, error) {
 	return l.Client.TTL(key).Result()
 }
--- rate_limiter_test.go
+++ rate_limiter_test.go
@@ -1,20 +1,58 @@
 package auth
 
 import (
+	"errors"
 	"testing"
 	"time"
 
 	"github.com/go-redis/redis/v7"
 )
 
-var url = "localhost:5973"
+var url = "localhost:6379"
 
-func TestRateLimiter(t *testing.T) {
+func getLimiter() Limiter {
 	rds := redis.NewClient(&redis.Options{
 		Addr:       url,
 		MaxRetries: 3,
 	})
 
-	limiter := RedisLimiter{Client: rds}
-	limiter.Limit("test_key", 1, time.Duration(time.Second))
+	limiter := NewRedisRateLimiter(rds)
+	return limiter
+}
+
+func TestLimit1Passes(t *testing.T) {
+	l := getLimiter()
+
+	if err := l.Limit("test", 1, 0); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestLimit2Fails(t *testing.T) {
+	l := getLimiter()
+	l.Limit("test", 1, time.Second)
+	if err := l.Limit("test", 1, time.Second); err == nil {
+		t.Fatal("expected err got nil")
+	}
+
+}
+
+func TestAsMaxAttemptsError(t *testing.T) {
+	l := getLimiter()
+	err := l.Limit("test", 0, time.Second)
+
+	var e MaxAttemptsError
+	if !errors.As(err, &e) {
+		t.Fatal("expected errors as to be true for max attempts error")
+	}
+}
+
+func TestRateLimiter(t *testing.T) {
+	l := getLimiter()
+
+	n := 2
+	for i := 0; i < n; i++ {
+		l.Limit("test_key", n, time.Duration(time.Second))
+	}
+
 }
